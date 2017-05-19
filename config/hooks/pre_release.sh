8928ed2077ab6f8e7043d0b904a7e351a98f55ae
jonasf <jonas.falkevik@mobilearts.com>
2017-05-19 10:09:46 +0200

    Rocksdb is compiled as a static lib.
    Having rocksdb as a static lib enable us to have test running from rebar.
    As well as we can upgrade the nif with new rocksdb in single file.
    Drawback is of course if we have multiple nifs using rocksdb. which is not the case right now.

--- config/hooks/pre_release.sh
+++ config/hooks/pre_release.sh
@@ -3,6 +3,5 @@
 ## Copy leveldb and rocksdb libraries
 SLIB=config/slib/
 mkdir -p $SLIB
-cp $REBAR_DEPS_DIR/erl_rocksdb/c_src/rocksdb/librocksdb.* $SLIB
 cp $REBAR_DEPS_DIR/erl_leveldb/c_src/leveldb/libleveldb.* $SLIB
 # End
