pundunlabs/pundun

commit 4b8621130b4e29c062d2ac8c510a8804711102f8

Copy vm.args to release_root_dir to survive upgrades

config/hooks/post_release.sh
@@ -19,12 +19,19 @@ fi
 
 ##GENERATE Node Name from path
 sed '/^NAME=/a \
-if [ -z "$NAME" ]; then\
-    NAME="pundun"`echo $HOSTNAME $RELEASE_ROOT_DIR | openssl sha1 | cut -b 10-15`\
-    echo "Generated node name $NAME"\
-    NAME_ARG="$NAME_TYPE $NAME"\
-    sed -i -e "s;$NAME_TYPE;$NAME_TYPE $NAME;" $VMARGS_PATH\
+if [ ! ${VMARGS_PATH} == ${RELEASE_ROOT_DIR}/vm.args ] ; then\
+\    echo "Copy ${VMARGS_PATH} to ${RELEASE_ROOT_DIR}/"\
+\    cp -p ${VMARGS_PATH} ${RELEASE_ROOT_DIR}/ \
+\    VMARGS_PATH=$(add_path vm.args)\
 fi\
+\
+if [ -z "$NAME" ]; then\
+\    NAME="pundun"`echo $HOSTNAME $RELEASE_ROOT_DIR | openssl sha1 | cut -b 10-15`\
+\    echo "Generated node name $NAME"\
+\    NAME_ARG="$NAME_TYPE $NAME"\
+\    sed -i -e "s;$NAME_TYPE;$NAME_TYPE $NAME;" $VMARGS_PATH\
+fi\
+\
 ' $s
 ## export PRODDIR
 ## gb_conf is using and dependent on PRODDIR env variable.
@@ -56,4 +63,5 @@ create_sym_links()
     done
 }
 create_sym_links
+
 # End

commit be1da3fdc634589c81a15b48588a4ddbdd0fd530

Fix string comparison bug introduced with 4b86211
Use '=' instead of '==' for string comparison.

config/hooks/post_release.sh
@@ -19,7 +19,7 @@ fi
 
 ##GENERATE Node Name from path
 sed '/^NAME=/a \
-if [ ! ${VMARGS_PATH} == ${RELEASE_ROOT_DIR}/vm.args ] ; then\
+if [ ! ${VMARGS_PATH} = ${RELEASE_ROOT_DIR}/vm.args ] ; then\
 \    echo "Copy ${VMARGS_PATH} to ${RELEASE_ROOT_DIR}/"\
 \    cp -p ${VMARGS_PATH} ${RELEASE_ROOT_DIR}/ \
 \    VMARGS_PATH=$(add_path vm.args)\