3d017a2963f62231fe9f2d0cc4eb50cad824e332
erdem <erdem.aksu@mobilearts.com>
2017-12-14 23:33:47 +0100

    Copy start script on post release hook.
    Instead of modifying start script by using sed, now we copy start script
    to bin/pundun and bin/pundun-<VERSION> files.

--- config/hooks/post_release.sh
+++ config/hooks/post_release.sh
@@ -1,50 +1,8 @@
 #!/bin/sh
 
-OS_NAME=$(uname -s)
-
-if [ $OS_NAME = "Darwin" ] ; then
-    alias sed="sed -i '' "
-else
-    alias sed='sed -i '
-fi
-
-## Use sed to inject customized code in start scripts.
-START_SCRIPTS=`find $REBAR_BUILD_DIR/rel -type f -name pundun`
+START_SCRIPTS=`find $REBAR_BUILD_DIR/rel/pundun/bin -type f -name 'pundun*'`
 for s in $START_SCRIPTS; do
-
-if [ $OS_NAME != "Darwin" ] ; then
-    sed 's/readlink/readlink -f/g' $s
-fi
-
-
-##GENERATE Node Name from path
-sed '/^NAME=/a \
-if [ ! ${VMARGS_PATH} = ${RELEASE_ROOT_DIR}/vm.args ] ; then\
-\    echo "Copy ${VMARGS_PATH} to ${RELEASE_ROOT_DIR}/"\
-\    cp -p ${VMARGS_PATH} ${RELEASE_ROOT_DIR}/ \
-\    VMARGS_PATH=$(add_path vm.args)\
-fi\
-\
-if [ -z "$NAME" ]; then\
-\    NAME="pundun"`echo $HOSTNAME $RELEASE_ROOT_DIR | openssl sha1 | cut -b 10-15`\
-\    echo "Generated node name $NAME"\
-\    NAME_ARG="$NAME_TYPE $NAME"\
-\    sed -i -e "s;$NAME_TYPE;$NAME_TYPE $NAME;" $VMARGS_PATH\
-fi\
-\
-' $s
-## export PRODDIR
-## gb_conf is using and dependent on PRODDIR env variable.
-sed '/^RELEASE_ROOT_DIR=/a \
-export PRODDIR=$RELEASE_ROOT_DIR\
-' $s
-
-## export PIPE_DIR
-## put run_erl pipes under the release
-sed '/PRODDIR=/a \
-export PIPE_DIR=$RELEASE_ROOT_DIR\/var\/pipes\/pundun\
-' $s
-
+    cp $REBAR_ROOT_DIR/config/pundun.start_script $s
 done
 
 ## Create symlinks to configuration files
