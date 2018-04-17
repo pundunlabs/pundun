6b496950e6ffc7e7e2090bbf3c51631488308a1b
erdem <erdem.aksu@mobilearts.com>
2018-04-17 12:31:30 +0200

    Use sed injection to modify start_script.
    Merging approach was not compatible with new rebar3 versions.
    Use 'noname' for sname parameter in vm.args which is due to change by injected script.

--- config/hooks/post_release.sh
+++ config/hooks/post_release.sh
@@ -1,32 +1,51 @@
 #!/bin/sh
+
 OS_NAME=$(uname -s)
+
 if [ $OS_NAME = "Darwin" ] ; then
     alias sed="sed -i '' "
 else
     alias sed='sed -i '
 fi
 
-#Specify input scripts (Rebar3 provided and our modified)
-START_SCRIPT=`find $REBAR_BUILD_DIR/rel/pundun/bin -type f -name pundun`
-CUSTOM_SCRIPT=$REBAR_ROOT_DIR/config/pundun.start_script
-#Specify output script file for merged content.
-MERGED_SCRIPT=$REBAR_ROOT_DIR/config/merged.start_script
-## Merge rebar3 script with our modified script.
-## Take theirs (Rebar3 provided) in case of conflict.
-diff -DVERSION1 $START_SCRIPT $CUSTOM_SCRIPT > $MERGED_SCRIPT
-sed '/^#ifndef VERSION1/d' $MERGED_SCRIPT
-sed '/^#else \/\* VERSION1 \*\//,/^#endif \/\* VERSION1 \*\//d' $MERGED_SCRIPT
-sed '/^#ifdef VERSION1/d' $MERGED_SCRIPT
-sed '/^#endif \/\* VERSION1 \*\//d' $MERGED_SCRIPT
-
-## Find target script files to replace.
-START_SCRIPTS=`find $REBAR_BUILD_DIR/rel/pundun/bin -type f -name 'pundun*'`
-## Replace target script files with the merged script.
+## Use sed to inject customized code in start scripts.
+START_SCRIPTS=`find $REBAR_BUILD_DIR/rel -type f -name pundun`
 for s in $START_SCRIPTS; do
-    cp $MERGED_SCRIPT $s
+
+if [ $OS_NAME != "Darwin" ] ; then
+    sed 's/readlink/readlink -f/g' $s
+fi
+
+
+##GENERATE Node Name from path
+sed '/^NAME=/a \
+if [ ! ${VMARGS_PATH} = ${RELEASE_ROOT_DIR}/vm.args ] ; then\
+\    echo "Copy ${VMARGS_PATH} to ${RELEASE_ROOT_DIR}/"\
+\    cp -p ${VMARGS_PATH} ${RELEASE_ROOT_DIR}/ \
+\    VMARGS_PATH=$(add_path vm.args)\
+fi\
+\
+if [ -z "$NAME" ] || [ "$NAME" = "noname" ]; then\
+\    NAME="pundun"`echo $HOSTNAME $RELEASE_ROOT_DIR | openssl sha1 | cut -b 10-15`\
+\    echo "Generated node name $NAME"\
+\    NAME_ARG="$NAME_TYPE $NAME"\
+\    sed -i -e "s;^$NAME_TYPE.*;$NAME_TYPE $NAME;" $VMARGS_PATH\
+fi\
+\
+' $s
+## export PRODDIR
+## gb_conf is using and dependent on PRODDIR env variable.
+sed '/^RELEASE_ROOT_DIR=/a \
+export PRODDIR=$RELEASE_ROOT_DIR\
+' $s
+
+## export PIPE_DIR
+## put run_erl pipes under the release
+sed '/PRODDIR=/a \
+export PIPE_DIR=$RELEASE_ROOT_DIR\/var\/pipes\/pundun\
+' $s
+
 done
-## Remove the merged script.
-rm $MERGED_SCRIPT
 
 ## Create symlinks to configuration files
 ## This makes easier access to configuration files.
